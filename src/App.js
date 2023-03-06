4247c23d3bf6aab51558b308a76d735973d1e188
NayeemL <111557614+NayeemL@users.noreply.github.com>
2023-03-06 22:23:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-toastify/dist/ReactToastify.css';
+import React, { Suspense } from "react";
+// import AdminRoutes from "./Routes/AdminRoutes";
+// import EmployerRoutes from './Routes/EmployerRoutes';
+
+import { Route, Routes } from "react-router-dom";
+// import ProtectedRoute from "./Routes/ProtectedRoute";
+// import Dashboard from "./component/AdminLayout/index";
+import LoginPage from "./pages/LoginPage/index";
+// import AdminLogin from "./pages/LoginPage/AdminLogin";
+// import DefaultLayout from "./pages/Employee/DefaultLayout";
+// import UserDefaultLayout from "./pages/User/DefaultLayout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ec Files</h1>
-      </header>
-    </div>
+    <Suspense fallback={<h1>Loading</h1>}>
+      <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      </Routes>
+      </Suspense>
   );
 }
 
